920ab57f62572141465c96e68d8f6bd7216b543c
Ali Moallemi <alimoallemi30@gmail.com>
2023-05-02 13:26:03 +0330

    render method added

--- HTML-ELEMENT/htmlElement.js
+++ HTML-ELEMENT/htmlElement.js
@@ -25,10 +25,38 @@ function HtmlSelectElement(...items) {
       console.log("Item Removed!");
     }
   };
+
+  this.render = function () {
+    const renderedElem = document.createElement("select");
+    for (let item in this.items) {
+      const innerElem = document.createElement("option");
+      innerElem.innerText = item;
+      renderedElem.appendChild(innerElem);
+    }
+    return renderedElem;
+  };
 }
 
 HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
 
+function HtmlImageElement(src) {
+  this.src = src;
+  this.render = function () {
+    const renderedElem = document.createElement("img");
+    renderedElem.src = this.src;
+    return renderedElem;
+  };
+}
+
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
+
 const elem = new HtmlElement();
 const selectElem = new HtmlSelectElement("1", "2", "3");
+const imageElem = new HtmlImageElement("#");
+
+const elements = [selectElem, imageElem];
+for (let elem of elements) {
+  console.log(elem.render());
+}
